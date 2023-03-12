916e9cec5b4c311035bfd3154f628982941ed8f2
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-12 14:36:53 -0500

    Created reverse-string.py

---
+++ problem-solving-problems-p1/reverse-string.py
@@ -0,0 +1,10 @@
+word = 'Nanette'
+
+
+def reverser(word):
+    reversed_word= ''
+    for i in range(len(word) -1, -1, -1):
+        return reversed_word
+    print(reversed_word)
+
+reverser(word)
