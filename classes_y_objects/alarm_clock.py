bb9759eb55fefaaa0b7c61f48c09fb599d611484
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-23 13:53:49 -0500

    Set up AlarmClock class in alarm_clock.py

---
+++ classes_y_objects/alarm_clock.py
@@ -0,0 +1,24 @@
+class AlarmClock: 
+    def __init__(self):
+        self.current_time = ''
+        self. alarm_status = 'on'
+        self.alarm_alert = ''
+        pass
+
+    def change_current_time(self):
+        #  I want the AlarmClock class to have a 
+        #  method to set (or change) the current time and print to the console the current time.
+        pass
+
+    def toggle_alarm(self):
+        # I want the AlarmClock class 
+        # to have a method to toggle the alarm on or off. 
+        pass
+
+    def set_current_alarm_time(self):
+        # I want the AlarmClock class 
+        # to have a method to set the current alarm time 
+        # and print to the console the current alarm time
+        pass
+
+# https://docs.google.com/document/d/1qe-YBxgL7rM4k-Pmpp3jyHY3XIDS9Hs0/edit
