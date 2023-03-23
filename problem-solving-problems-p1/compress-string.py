ee9fafaa9938d940512304ca031e4b9cc45192d8
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-23 16:46:39 -0500

    Added comments in compress-string.py

--- problem-solving-problems-p1/compress-string.py
+++ problem-solving-problems-p1/compress-string.py
@@ -5,15 +5,15 @@ count_this_variable = input('\nType any variable to count its types: \n')
 
 def type_counter(og_variable):
     final_result = ''
-    letter_counter = 1
+    letter_counter = 1 # define a letter counter that begins at 1
     for i in range(len(og_variable)):
         if i == len(og_variable) - 1:
             final_result += str(letter_counter) + og_variable[i]
-        elif og_variable[i] == og_variable[i + 1]:
+        elif og_variable[i] == og_variable[i + 1]: # condition if variable at that index == variable ahead of the index by 1 then we + 1 to letter counter
             letter_counter += 1
         elif og_variable[i] != og_variable[i + 1]:
             final_result += str(letter_counter) + og_variable[i]
-            letter_counter = 1
+            letter_counter = 1 # letter counter resets back to 1 everytime all the same variables are accounted for
     print(final_result)
 
 
