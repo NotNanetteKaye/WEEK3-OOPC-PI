8fc8efc4d05b6ee0fa9b9d4f3f4db72efa1b690e
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-28 18:01:25 -0500

    Solved 49 Leetcode

---
+++ problemas_leetcode/49_anagrams.py
@@ -0,0 +1,20 @@
+# Given an array of strings, group the anagrams together. You can return the answer in any order.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+# typically using all the original letters exactly once.
+
+from collections import defaultdict
+
+strs = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat']
+
+anagram_map = defaultdict(list)
+result = []
+
+for s in strs:
+    sorted_s = tuple(sorted(s))
+    anagram_map[sorted_s].append(s)
+
+
+for value in anagram_map.values():
+    result.append(value)
+
+print(result)
